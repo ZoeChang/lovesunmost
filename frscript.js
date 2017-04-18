3c2d1f71b45f78aef6db27e3ec443d752f3cf07d
Zoe <1111@22222.com>
2017-04-18 19:39:25 +0800

    -new version

---
+++ frscript.js
@@ -0,0 +1,29 @@
+// carousel 調整速度
+$(function(){
+    $('.carousel').carousel({interval: 200});
+});
+
+
+// 縮圖變大
+
+/*
+$(function(){
+   $('.bigbox1 img').onmousemove({
+       var picsrc;
+       picsrc = 'hello';
+       document.getElementById("demo").innerHTML = picsrc;
+   }); 
+});
+
+*/
+
+$(function(){
+    $('.bigbox1 img').mouseover(function(){
+        var smsrc = $(this).attr("src").substr(18,1);
+        var picsrc = 'data/newarrival/00' + smsrc + 'en.jpg';
+        var $illus = $('#illus');
+        
+        $illus.attr("src" , picsrc );
+    });
+       
+}); 
